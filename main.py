77d732aa71476e63e05df76f9eff24aa0bd16089
yup8j <jiahanshi@gmail.com>
2021-09-21 00:31:32 +0800

    feat: Basic clfs; delete: csv files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 column_names = ['age', 'workclass', 'fnlwgt', 'education', 'educational-num', 'marital-status', 'occupation',
                 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
@@ -30,6 +32,34 @@ def clean_data(df: pd.DataFrame):
     return df
 
 
-test = clean_data(test)
-train = clean_data(train)
+# adult.test does not contain "Holand-Netherlands"
+df = pd.concat([train, test], axis=0)
+df = clean_data(df)
+x = df.drop(['income'], axis=1)
+x = preprocessing.scale(x)
+
+y = df['income']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+y_lr = lr.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy score of Logistic Regression is {}".format(accuracy_score(y_true=y_test, y_pred=y_lr)))
+
+
+dt = DecisionTreeClassifier()
+dt.fit(x_train, y_train)
+y_dt = dt.predict(x_test)
+print("Accuracy score of Decision Tree Classifier is {}".format(accuracy_score(y_true=y_test, y_pred=y_dt)))
+
+
+# from xgboost import XGBClassifier
+#
+# xgb = XGBClassifier()
+# xgb.fit(x_train,y_train)
+# y_xgb = xgb.predict(x_test)
+# print("Accuracy score of XGBClassifier is {}".format(accuracy_score(y_true=y_test, y_pred=y_xgb))) #0.8600969084829045
+
 
