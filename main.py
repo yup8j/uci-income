45ca9bf4c9c8d5c9650fde0da28291170bacdc70
yup8j <jiahanshi@gmail.com>
2021-09-20 17:14:12 +0800

    feat: Data Clean

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 
-# 读入数据，header=None表示没有列名
-df = pd.read_csv('./adult.data', header=None)
-
-# 展示前5行数据
-print(df.head())
-
-# 利用pandas分析列性质，object为非数值型数据
-print(df.info())
-
-# 我抄的，从adult.names
 column_names = ['age', 'workclass', 'fnlwgt', 'education', 'educational-num', 'marital-status', 'occupation',
-                'relationship', 'race', 'gender', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+                'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
                 'income']
-# 给列取名
-
-df.columns = column_names
-print(df.info())
-# describe一下数值型数据
-print(df.describe())
+train = pd.read_csv('./adult.data', header=None, na_values='?', names=column_names)
+test = pd.read_csv('./adult.test', header=None, skiprows=1, names=column_names)
 
 
-def one_hot_convert(dataframe: pd.DataFrame, df_col):
-    """
-    One hot 转换器
-    :param dataframe: 原始dataframe
-    :param df_col: 需要转换的列名
-    :return:
-    """
-    # drop掉原来的列
-    df_drop = dataframe.drop(columns=df_col, axis=1)
-    # 使用get_dummies方法进行onehot，参考 https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
-    df_converted = pd.get_dummies(dataframe[df_col])
+def clean_data(df: pd.DataFrame):
+    dummy_features = ['workclass', 'marital-status', 'occupation', 'relationship', 'race',
+                      'native-country']
+    df.replace(' ?', np.nan, inplace=True)
+    df['income'] = df['income'].apply(lambda x: 1 if x == ' >50K' else 0)
+    df['sex'] = df['sex'].apply(lambda x: 1 if x == ' Male' else 0)
+    for feature in dummy_features:
+        df = pd.concat([df, pd.get_dummies(df[feature])], axis=1)
+        df.drop(feature, axis=1, inplace=True)
+    education = ["Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", "11th", "12th", "HS-grad", "Prof-school",
+                 "Assoc-acdm", "Assoc-voc", "Some-college", "Bachelors", "Masters", "Doctorate"]
+    education_map = {}
+    num = 0
+    for e in education:
+        education_map[e] = num
+        num += 1
+    df['education'] = df['education'].str.strip().map(education_map)
+    return df
 
-    return pd.concat([df_drop, df_converted], axis=1, join='inner') #返回拼接结果
 
+test = clean_data(test)
+train = clean_data(train)
 
-# 原始数据
-print(df.head())
-
-# 转换workclass
-df = one_hot_convert(df, "workclass")
-print(df.head())
